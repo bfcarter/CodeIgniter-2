95897c3399b42d2263bc8b4dd34252c7ef1768f9
bfcarter <bfcarter@fullsail.edu>
2016-05-19 19:55:29 -0500

    update page

--- application/controllers/update_ctrl.php
+++ application/controllers/update_ctrl.php
@@ -5,13 +5,13 @@ parent::__construct();
 $this->load->model('update_model');
 }
 function show_name() {
-$id = $this->uri->segment(3);
+$name = $this->uri->segment(3);
 $data['users'] = $this->update_model->show_users();
 $data['single_user'] = $this->update_model->show_name($name);
-$this->load->view('update_view', $data);
+$this->load->view('memberarea_view', $data);
 }
 function update_name1() {
-$id= $this->input->post('uname');
+$name= $this->input->post('uname');
 $data = array(
 'name' => $this->input->post('uname'),
 'email' => $this->input->post('uemail'),
@@ -22,4 +22,3 @@ $this->show_name();
 }
 }
 ?>
-
