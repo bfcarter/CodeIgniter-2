3c74b54ffabe039638a06b582f5492079a602d8d
bfcarter <bfcarter@fullsail.edu>
2016-05-21 21:04:22 -0500

    add id

--- application/controllers/update_ctrl.php
+++ application/controllers/update_ctrl.php
@@ -4,24 +4,24 @@ function __construct(){
 parent::__construct();
 $this->load->model('update_model');
 }
-function show_name() {
+function show_id() {
 $name = $this->uri->segment(3);
 $data['users'] = $this->update_model->show_users();
 $data['single_user'] = $this->update_model->show_name($name);
 $this->load->view('memberarea_view', $data);
 }
-function update_name1() {
-$name= $this->input->post('uname');
-echo "uname";
+function update_id1() {
+$name= $this->input->post('uid');
 $data = array(
 
 'name' => $this->input->post('uname'),
 'email' => $this->input->post('uemail'),
 'password' => $this->input->post('upassword')
 );
-$this->update_model->update_name1($name,$data);
+$this->update_model->update_id1($uid,$data);
 $this->show_name();
 $this ->load->view('memberarea_view', $data);
 }
 }
 ?>
+
